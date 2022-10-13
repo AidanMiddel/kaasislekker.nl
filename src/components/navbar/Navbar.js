4ae700d107bc009db96119f8e17a2dc95858a2b3
AidanMiddel <70641287+AidanMiddel@users.noreply.github.com>
2022-10-13 12:37:37 +0200

    edit style navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,12 +4,11 @@ const Navbar = () => {
     return (
         <nav className="navigationBar">
             <li><img className="logo" src="logo.png" alt="" /></li>
-            <li>Vacatures zoeken</li>
-            <li>Bedrijfsreviews</li>
-            <li>Salarischeck</li>
-            <li>Upload je cv</li>
-            <li>Inloggen</li>
-            <li>Werkgevers / Plaats vacature</li>
+            <li>Over</li>
+            <li>Features</li>
+            <li>Prijs</li>
+            <li>Contact</li>
+            <li className="navigationBar--login">Inloggen</li>
         </nav>
     )
 }
